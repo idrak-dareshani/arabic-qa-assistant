c5cec8faba84d64cf932e0ceb20126d80c43e739
idrak <idrak@winit.biz>
2025-05-29 14:12:36 +0500

    applied tavily ai for web searching

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pdf_loader import extract_text_from_pdf
 from vector_store import create_vector_store, load_vector_store
 from qa_engine import answer_question
+from web_fallback import search_web, search_tool
 
 pdf_path = "data/Dream_Textbook.pdf"
 text = extract_text_from_pdf(pdf_path)
@@ -15,3 +16,10 @@ while True:
         break
     answer = answer_question(db, q)
     print("Answer:", answer)
+
+    fallback = input("\n Do you want more info from the web? (yes/no): ").lower()
+    if fallback == "yes":
+        #web_info = search_web(q)
+        web_info = search_tool(q)
+        print("\n Info from the web:")
+        print(web_info)
