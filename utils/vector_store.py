91e6632b84c890719c811f42ee0dba54ea074406
Idrak Dareshani <idrak@winit.biz>
2025-05-30 19:21:26 +0500

    added option to load more than 1 file

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -12,15 +12,23 @@ embeddings = OpenAIEmbeddings()
 def create_vector_store(pdf_path="data/Dream_Textbook.pdf", index_path="faiss_index"):
     #splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     #docs = splitter.create_documents([text])
-    loader = PyPDFLoader(pdf_path)
-    pages = loader.load()
+    #loader = PyPDFLoader(pdf_path)
+    #pages = loader.load()
+
+    pdf_files = ["data/Section1.pdf", "data/Section2.pdf", "data/Section3.pdf"]
+
+    all_docs = []
+    for file in pdf_files:
+        loader = PyPDFLoader(file)
+        docs = loader.load()
+        all_docs.extend(docs)
 
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
         chunk_overlap=100,
         separators=["\n", ".", "،", " "]
     )
-    chunks = splitter.split_documents(pages)
+    chunks = splitter.split_documents(all_docs)
 
     db = FAISS.from_documents(chunks, embeddings)
     db.save_local(index_path)
